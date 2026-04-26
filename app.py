bf448533cf429a0a8f753a17f760d17a781374d6
yogesvhshinde19 <128024576+yogeshvshinde@users.noreply.github.com>
2026-04-26 21:20:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
-import certifi
+#import certifi
 import os
 
 # Load env vars
@@ -14,7 +14,7 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 # Use certifi CA bundle explicitly for cross-platform TLS reliability
 # (notably fixes common macOS certificate verification failures).
-mongo = PyMongo(app, tlsCAFile=certifi.where())
+#mongo = PyMongo(app, tlsCAFile=certifi.where())
 
 # Home page -> list students
 @app.route('/')
